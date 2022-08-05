ffafe5ccd0f0cc16b97ef48f92d32ff8f97670e4
nishant <nishantshah1729@gmail.com>
2022-08-05 08:51:56 +0530

    initial

--- GetSubsequence.java
+++ GetSubsequence.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+
 public class GetSubsequence {
     public static void main(String[] args) {
+        String s="pep";
+        System.out.println(getss(s));
+    }
 
+    private static ArrayList<String> getss(String s) {
+        if(s.isEmpty()){
+            ArrayList<String> bres=new ArrayList<>();
+            bres.add("");
+            return bres;
+        }
+        char c=s.charAt(0);
+        ArrayList<String> ros=getss(s.substring(1));
+        ArrayList<String> ans=new ArrayList<>();
+        for(String x:ros){
+            ans.add(c+x);
+            ans.add(x);
+        }
+        return ans;
     }
 }
